71f5dd430c4f606c66f9a3b4f39c022568e9357f
gspider8 <83229711+gspider8@users.noreply.github.com>
2024-10-14 16:26:20 -0400

    working rewrite

--- update_html.py
+++ update_html.py
@@ -11,8 +11,6 @@ js = {
     "preceding_spaces": None
 }
 
-css_replacement = []
-js_replacement = []
 static_file_directory = "portfolio/fcc-javascript"
 
 
@@ -23,46 +21,52 @@ def leading_spaces(string):
 # Loop through each template html file to update css and js references
 counter = 0
 for file_obj in Path("templates").iterdir():
+    # Init
+    css_replacement = []
+    js_replacement = []
+
+    # Limiter
     counter += 1
     if counter > 1:
         break
 
-    # Patterns for regex search
-    js_pattern = re.compile("script.js")
-    css_pattern = re.compile("styles.css")
-
-    # Loop through each line and search
-    line_counter = 0
+    # Read file to memory
+    lines = []
     with open(str(file_obj.__fspath__()), "r") as r_file:
         for line in r_file:
+            lines.append(line)
 
-            print(f"{line_counter}: {line}")
+    # Determine lines that JS and CSS are referenced on
+    line_counter = 0
+    for line in lines:
+        if re.compile("script.js").search(line):
+            js["line_in_html"] = line_counter
+            js["preceding_spaces"] = leading_spaces(line)
 
-            if js_pattern.search(line):
-                js["line_in_html"] = line_counter
-                js["preceding_spaces"] = leading_spaces(line)
-                print(js)
-            elif css_pattern.search(line):
-                css["line_in_html"] = line_counter
-                css["preceding_spaces"] = leading_spaces(line)
-                print(css)
+        elif re.compile("styles.css").search(line):
+            css["line_in_html"] = line_counter
+            css["preceding_spaces"] = leading_spaces(line)
 
-            line_counter += 1
+        line_counter += 1
 
     # Replacement Strings for Django Templates
     project_name = file_obj.name.split('.')[0]
-    css_replacement.append("""<link type ="text/css" rel="stylesheet" href="{% static""")
-    css_replacement.append(f"'{static_file_directory}/{project_name}.css'")
-    css_replacement.append("""%}" />""")
+    css_replacement.append(css["preceding_spaces"] * ' ')
+    css_replacement.append('<link type ="text/css" rel="stylesheet" href="{% static')
+    css_replacement.append(f" '{static_file_directory}/{project_name}.css' ")
+    css_replacement.append('%}" />\n')
 
-    js_replacement.append("""<script src="{% static""")
-    js_replacement.append(f"'{static_file_directory}/{project_name}.js'")
-    js_replacement.append("""%}"></script>""")
+    js_replacement.append(js["preceding_spaces"] * " ")
+    js_replacement.append('<script src="{% static')
+    js_replacement.append(f" '{static_file_directory}/{project_name}.js' ")
+    js_replacement.append('%}"></script>\n')
 
-    # write new data in
-    # print((css["preceding_spaces"] * " ") + " ".join(css_replacement))
+    # rewrite file with new js and css references
     with open(str(file_obj.__fspath__()), "w") as w_file:
-
-        pass
-
-    print(line_counter)
+        for index in range(len(lines)):
+            if index == css["line_in_html"]:
+                w_file.write("".join(css_replacement))
+            elif index == js["line_in_html"]:
+                w_file.write("".join(js_replacement))
+            else:
+                w_file.write(lines[index])
